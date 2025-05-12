0c06863dfb538734246690b39956834d02bb4b7a
Eyoba_Smax <eyobsmax@gmail.com>
2025-05-12 13:44:11 +0300

    Examples

--- auth/auth.js
+++ auth/auth.js
@@ -2,6 +2,15 @@ import bcrypt from "bcrypt";
 
 async function checkUser(id, username, password) {
   //!Naomi's part
+  //?fetch user from DB using ORM()
+  const userFromDb = ""; //!find user from users table using username and id
+  if (!userFromDb) {
+    return { success: false, message: "User not found!" };
+  }
+  const checkPassword = await bcrypt.compare(password, userFromDb.password); //?compare the hashed password with the one we get from the DB
+  if (!checkPassword) {
+    return { success: false, message: "Incorrect password" };
+  }
 
   return { success: true, message: "User authenticated" };
 }
