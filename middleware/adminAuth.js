6fcc50b41dcde6e6bbfb84453222a2fcb99d23be
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-06-13 11:46:34 +0300

    Upsating thetypo inside buildToken function

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -4,20 +4,18 @@ dotenv.config();
 
 function buildToken(res, studentid, username) {
   const generalAdmin = process.env.ADMIN_USERNAME;
-  const isAdmin = username === generalAdmin ? true : false;
-  const token = jwt.sign(
-    { studentid, isAdmin, generalAdmin },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "3d",
-    }
-  );
+  const isAdmin = username === generalAdmin;
+
+  const token = jwt.sign({ studentid, isAdmin }, process.env.JWT_SECRET, {
+    expiresIn: "3d",
+  });
+
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",
     path: "/",
-    maxAge: 3 * 24 * 60 * 60 * 1000,
+    maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
   });
 }
 
